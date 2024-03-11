7057643685030f465283b575fb1694b67c4ca800
root <kokidanny15@gmail.com>
2024-03-11 15:25:29 +0300

    First API

---
+++ controllers/AppController.js
@@ -0,0 +1,27 @@
+const dbClient = require('../utils/db');
+
+class AppController {
+  static async getStatus(req, res) {
+    try {
+      const redisStatus = true;
+      const dbStatus = await dbClient.isAlive();
+      res.status(200).json({ redis: redisStatus, db: dbStatus });
+    } catch (error) {
+      console.error('Error in getStatus:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+
+  static async getStats(req, res) {
+    try {
+      const usersCount = await dbClient.nbUsers();
+      const filesCount = await dbClient.nbFiles();
+      res.status(200).json({ users: usersCount, files: filesCount });
+    } catch (error) {
+      console.error('Error in getStats:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+}
+
+module.exports = AppController;
