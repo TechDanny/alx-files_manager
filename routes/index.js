3ef31d781b2ec12759cc9400cf8014c19db4fb5f
root <kokidanny15@gmail.com>
2024-03-12 02:53:54 +0300

    File data

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,7 @@ router.post('/files', FilesController.postUpload);
 router.get('/files/:id', FilesController.getShow);
 router.get('/files', FilesController.getIndex);
 router.put('/files/:id/publish', FilesController.putPublish);
-router.get('/files/:id/data', FilesController.getFile);
 router.put('/files/:id/unpublish', FilesController.putUnpublish);
+router.get('/files/:id/data', FilesController.getFile);
 
 module.exports = router;
